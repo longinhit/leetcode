fc1e249332c268d99f0cb4a90a76967bb96f5d47
焦久隆 <jiaojiulong@bytedance.com>
2019-03-13 10:45:48 +0800

    accept 203
    Change-Id: I4f728f882ac6223203d5b43f3ee3eaf7a77a4fe2

--- 203.remove-linked-list-elements.go
+++ 203.remove-linked-list-elements.go
@@ -21,6 +21,7 @@
  *
  *
  */
+
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -29,19 +30,80 @@
  * }
  */
 
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
 
-func main() {
-	fmt.Printf("dddddd\n")
-	removeElements()
-}
+// func main() {
+// 	head := &ListNode{
+// 		Val: 1,
+// 		Next: &ListNode{
+// 			Val: 2,
+// 			Next: &ListNode{
+// 				Val: 6,
+// 				Next: &ListNode{
+// 					Val: 3,
+// 					Next: &ListNode{
+// 						Val: 4,
+// 						Next: &ListNode{
+// 							Val: 5,
+// 							Next: &ListNode{
+// 								Val: 6,
+// 							},
+// 						},
+// 					},
+// 				},
+// 			},
+// 		},
+// 	}
+// 	fmt.Printf("dddddd\n")
+// 	newHead := removeElements(head, 6)
+// 	// newHead := head
+// 	printLinks(newHead)
+// }
+
+// func printLinks(head *ListNode) *ListNode {
+// 	node := head
+// 	for {
+// 		if node != nil {
+// 			fmt.Printf("%d----->", node.Val)
+// 			node = node.Next
+// 		} else {
+// 			break
+// 		}
+// 	}
+// 	fmt.Printf("\n")
+// 	return head
+// }
+
 func removeElements(head *ListNode, val int) *ListNode {
-
+	node := head
+	var preNode *ListNode
+	for {
+		if node != nil {
+			if node.Val == val {
+				if node.Next == nil {
+					if preNode == nil {
+						return nil
+					}
+					preNode.Next = nil
+					node = nil
+				} else {
+					node.Val = node.Next.Val
+					node.Next = node.Next.Next
+				}
+			} else {
+				preNode = node
+				node = node.Next
+			}
+		} else {
+			break
+		}
+	}
+	return head
 }
---
+++ 204.count-primes.go
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=204 lang=golang
+ *
+ * [204] Count Primes
+ *
+ * https://leetcode.com/problems/count-primes/description/
+ *
+ * algorithms
+ * Easy (28.31%)
+ * Total Accepted:    216.9K
+ * Total Submissions: 765.3K
+ * Testcase Example:  '10'
+ *
+ * Count the number of prime numbers less than a non-negative number, n.
+ *
+ * Example:
+ *
+ *
+ * Input: 10
+ * Output: 4
+ * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+ *
+ */
+func countPrimes(n int) int {
+
+}
+
