d4fc16c17b97eedccab918a1b4f724a86dc3057f
焦久隆 <jiaojiulong@bytedance.com>
2019-03-14 10:40:45 +0800

    accept 219
    Change-Id: Ic0323b14f82ef241f3e2dd2c331d2e46b266d1e6

--- 219.contains-duplicate-ii.go
+++ 219.contains-duplicate-ii.go
@@ -14,36 +14,53 @@
  * Given an array of integers and an integer k, find out whether there are two
  * distinct indices i and j in the array such that nums[i] = nums[j] and the
  * absolute difference between i and j is at most k.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: nums = [1,2,3,1], k = 3
  * Output: true
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: nums = [1,0,1,1], k = 1
  * Output: true
- * 
- * 
- * 
+ *
+ *
+ *
  * Example 3:
- * 
- * 
+ *
+ *
  * Input: nums = [1,2,3,1,2,3], k = 2
  * Output: false
- * 
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
+ *
  */
-func containsNearbyDuplicate(nums []int, k int) bool {
-    
-}
 
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	fmt.Printf("%v\n", containsNearbyDuplicate([]int{1, 2, 3, 1, 2, 3}, 2))
+// }
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	length := len(nums)
+	for i := 0; i < length; i++ {
+		for j := 1; j <= k; j++ {
+			if i+j < length && nums[i] == nums[i+j] {
+				return true
+			}
+		}
+	}
+	return false
+}
